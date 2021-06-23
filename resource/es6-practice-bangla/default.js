1fe42865a34a025e402ea85f75ad106f4a061466
MonadWizard <monad.wizard.r@gmail.com>
2021-06-23 23:51:04 +0600

    add es6

--- resource/es6-practice-bangla/default.js
+++ resource/es6-practice-bangla/default.js
@@ -1,6 +1,9 @@
-function add(num1, num2 = 20){
-    return num1 + num2;
+function add(num1, num2 = 20) {
+    return num1 + num2
 }
 
-const total = add(15, 1);
-console.log(total);
+const total = add(15)
+console.log(total)
+
+const total2 = add(15, 10)
+console.log(total2)
