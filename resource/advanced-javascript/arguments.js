51ac15e9fafc1e18f70bdd2f4d21f3c323ad3034
MonadWizard <monad.wizard.r@gmail.com>
2021-07-03 16:29:42 +0600

    update resource

--- resource/advanced-javascript/arguments.js
+++ resource/advanced-javascript/arguments.js
@@ -1,6 +1,8 @@
-function add(num1, num2){
-    console.log([...arguments]);
-    return num1 + num2 + arguments[2];
+function add(num1, num2) {
+    console.log([...arguments])
+    return num1 + num2 + arguments[2]
 }
-const result = add(2, 3, 5, 7);
-console.log(result);
+const result = add(2, 3, 5, 7)
+console.log(result)
+
+
